65ecd17bf1570e05156068dcfa8719064fb3c3bb
the-dakhinia <www.swastiknayak.d@gmail.com>
2024-12-19 17:51:03 +0530

    third commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  // socket.emit("message", msg);
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit("message", msg)
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', function (req, res) {
+})
